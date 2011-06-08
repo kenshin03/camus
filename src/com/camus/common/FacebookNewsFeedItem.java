32b6c30618f881f1f4c4d005d516f1b93270c6ea
kenshin03 <kenny.sm.tang@gmail.com>
2011-06-08 16:43:39 +0800

    added facebook news feed parsing

---
+++ src/com/camus/common/FacebookNewsFeedItem.java
@@ -0,0 +1,28 @@
+package com.camus.common;
+
+public class FacebookNewsFeedItem extends FeedStory{
+	
+	private int likesCount;
+	private int commentsCount;
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getLikesCount() {
+		return likesCount;
+	}
+	public void setLikesCount(int likesCount) {
+		this.likesCount = likesCount;
+	}
+	public int getCommentsCount() {
+		return commentsCount;
+	}
+	public void setCommentsCount(int commentsCount) {
+		this.commentsCount = commentsCount;
+	}
+
+}
